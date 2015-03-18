9cda742960846500f3ededab4ac69f61db6090f6
Hiroshi Miura <miurahr@linux.com>
2015-03-19 08:02:08 +0900

    fix permission error
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- utils/run_hubot.sh
+++ utils/run_hubot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo docker run -d -p 22 -p 3000:3000 miurahr/hubot-lingr
 DPID=`sudo docker ps -q -l`
-echo $DPID > /run/docker-hubot-lingr.pid
+echo $DPID > /tmp/docker-hubot-lingr.pid
+sudo mv /tmp/docker-hubot-lingr.pid /run/
