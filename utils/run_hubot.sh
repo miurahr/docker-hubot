c6aa51c5dace027a7bf83b0817108b65a27a8f1b
Hiroshi Miura <miurahr@linux.com>
2015-03-19 09:12:00 +0900

    util update
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- utils/run_hubot.sh
+++ utils/run_hubot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo docker run -d -p 22 -p 3000:3000 miurahr/hubot-lingr
+sudo docker run -d -p 22 miurahr/hubot-lingr
 DPID=`sudo docker ps -q -l`
 echo $DPID > /tmp/docker-hubot-lingr.pid
 sudo mv /tmp/docker-hubot-lingr.pid /run/
