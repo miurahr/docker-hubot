a79a383bcbd09e326588ca2edd9378889a1f2eb2
Hiroshi Miura <miurahr@linux.com>
2015-03-20 06:49:31 +0900

    hubot-trello variables
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- runner.sh
+++ runner.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-sudo sed -ri "s/HUBOT_LINGR_BOT=/HUBOT_LINGR_BOT=${LINGR_BOT_ID}/g" /etc/supervisor/conf.d/hubot.conf
-sudo sed -ri "s/HUBOT_LINGR_SECRET=/HUBOT_LINGR_SECRET=${LINGR_BOT_SECRET}/g" /etc/supervisor/conf.d/hubot.conf
+
+HUBOT_CONF=/etc/supervisor/conf.d/hubot.conf
+
+
+# hubot_lingr environment
+sudo sed -ri "s/HUBOT_LINGR_BOT=/HUBOT_LINGR_BOT=${HUBOT_LINGR_BOT}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_LINGR_SECRET=/HUBOT_LINGR_SECRET=${HUBOT_LINGR_BOT_SECRET}/g" $HUBOT_CONF
+
+# hubot_trello environment
+sudo sed -ri "s/HUBOT_TRELLO_KEY=/HUBOT_TRELLO_KEY=${HUBOT_TRELLO_KEY}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_TRELLO_TOKEN=/HUBOT_TRELLO_TOKEN=${HUBOT_TORELLO_TOKEN}/g" $HUBOT_CONF
+sudo sed -ri "s/HUBOT_TRELLO_BOARD=/HUBOT_TRELLO_BOARD=${HUBOT_TRELLO_BOARD}/g" $HUBOT_CONF
+
 supervisord -n
--- utils/run_hubot.sh
+++ utils/run_hubot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo docker run -d -p 22 miurahr/hubot-lingr
+sudo docker run -d -p 22 -e "HUBOT_LINGR_BOT=bot_id" -e "HUBOT_LINGR_SECRET=xxxxxxxxx" \
+  -e "HUBOT_TRELLO_KEY=key" -e "HUBOT_TRELLO_TOKEN=secret" -e "HUBOT_TRELLO_BOARD=boardid" miurahr/hubot-lingr
 DPID=`sudo docker ps -q -l`
 echo $DPID > /tmp/docker-hubot-lingr.pid
 sudo mv /tmp/docker-hubot-lingr.pid /run/
